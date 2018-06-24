347b64bf9371be7bdb0bf0d630e22b15a6bc206c
Shane Havers <sahesmh@outlook.com>
2018-06-24 11:08:23 +1000

    Updated error throwing to allow including the part number (Gearbox 1, Radiator 2 etc.) in output message

--- software/QUT_ChassisControler/includes/chassisError.c
+++ software/QUT_ChassisControler/includes/chassisError.c
@@ -13,31 +13,49 @@
  * 			error_code		- Error to report
  * Returns: none
  * 
- * Not implemented yet
+ * Generates an error message, and takes the necessary actions depending on the severity and error.
+ * Uses 0 as the part number
  **/
 void throw_error_code(uint16_t severity, uint16_t error_code)
 {	
+	throw_error_code_with_number(severity, error_code, 0);
+}
+
+/**
+ * throw_error_code_with_number()
+ * Input:	severity	- Severity of the error (INFO, WARN, or ERROR)
+ * 			error_code	- Error to report
+ * 			part_number - The number of the part generating the error (i.e. Gearbox 1, Radiator 2 etc.)
+ * Returns: none
+ * 
+ * Generates an error message, and takes the necessary actions depending on the severity and error
+ * 
+ * */
+void throw_error_code_with_number(uint16_t severity, uint16_t error_code, uint16_t part_number) 
+{
 	switch (error_code) {
 		// Example - Error Codes TBD
 		case ERROR_BRAKE_PRESSURE:					
 			switch (severity) {
 				case INFO:
 				// Logging behaviour for Info-level logs
-				send_error_message(INFOSTRING, "Brake Pressue Normal")
+				send_error_message(INFOSTRING, "Brake %d Pressue Normal")
 				break;
 
 				case WARN;
 				// Loggging behaviour for Warning-level logs
-				send_error_message(WARNSTRING, "Break pressure abnormal!");
+				send_error_message(WARNSTRING, "Brake %d pressure abnormal!");
 				break;
 
 				case ERROR:
 				// Loggign behaviour for Error-level logs
-				send_error_message(ERRORSTRING, "BREAK PRESSURE CRITICAL");
+				send_error_message(ERRORSTRING, "BRAKE %d PRESSURE CRITICAL");
 				break;
 			}			
 		break;
 		
+		// Template for new error codes, copy and uncomment to use
+		// If the description contains a single %d, it can be used to indicate a part number
 		// case ERROR_CODE:					
 		// 	switch (severity) {
 		// 		case INFO:
@@ -60,22 +78,31 @@ void throw_error_code(uint16_t severity, uint16_t error_code)
 		// 	}			
 		// break;
 
-	}		
+	}
 }
 
 /**
  * send_error_message()
- * Input: 	start	- String representation of log level
- * 			message	- Message to send - should not exceed 40 characters
+ * Input: 	start		- String representation of log level
+ * 			message		- Message to send - should not exceed 40 characters
+ * 			part_number - The number of the part generating the error (i.e. Gearbox 1, Radiator 2 etc.)
  * Returns: none
  * 
  * Formats a 50-character long log message, and sends it over UART.
  * */
-void send_error_message(char start[], char message[]) 
+void send_error_message(char start[], char message[], uint16_t part_number) 
 {
+	unsigned char *messageWithNumber;
 	unsigned char *errorData;
-	snprintf(errorData, 50, "%s %s", start, message);
-	uart_send_data(errorData, 50);
+	
+	// Format error message with part number if given
+	snprintf(messageWithNumber, MAX_ERROR_MESSAGE_LENGTH, message, part_number);
+
+	// Create error data
+	snprintf(errorData, MAX_ERROR_MESSAGE_LENGTH + 10, "%s %s", start, messageWithNumber);
+	
+	// Send error data over UART
+	uart_send_data(errorData, MAX_ERROR_MESSAGE_LENGTH + 10);
 }
 
 /**
--- software/QUT_ChassisControler/includes/chassisError.h
+++ software/QUT_ChassisControler/includes/chassisError.h
@@ -11,37 +11,22 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+#define MAX_ERROR_MESSAGE_LENGTH 40
+
 #define ERROR_PEDALS 			1
 #define ERROR_BRAKE_PRESSURE 	2
 #define ERROR_INVERTER_RESPONSE 3
 #define ERROR_CAN2_RESPONSE		4
 
 /* Gearbox Error Codes */
-#define GEARBOX_1_TEMPERATURE_GOOD 5
-#define GEARBOX_1_TEMPERATURE_HIGH 6
-#define GEARBOX_1_TEMPERATURE_LOW 7
+#define GEARBOX_TEMPERATURE_GOOD 5
+#define GEARBOX_TEMPERATURE_HIGH 6
+#define GEARBOX_TEMPERATURE_LOW 7
 
-#define GEARBOX_2_TEMPERATURE_GOOD 8
-#define GEARBOX_2_TEMPERATURE_HIGH 9
-#define GEARBOX_2_TEMPERATURE_LOW 10
-
-#define GEARBOX_3_TEMPERATURE_GOOD 11
-#define GEARBOX_3_TEMPERATURE_HIGH 12
-#define GEARBOX_3_TEMPERATURE_LOW 13
-
-#define GEARBOX_4_TEMPERATURE_GOOD 14
-#define GEARBOX_4_TEMPERATURE_HIGH 15
-#define GEARBOX_4_TEMPERATURE_LOW 16
-
-#define RADIATOR_1_TEMPERATURE_GOOD 17
-#define RADIATOR_1_TEMPERATURE_HIGH 18
-#define RADIATOR_1_TEMPERATURE_LOW 19
-
-#define RADIATOR_2_TEMPERATURE_GOOD 17
-#define RADIATOR_2_TEMPERATURE_HIGH 18
-#define RADIATOR_2_TEMPERATURE_LOW 19
-
-
+/* Radiator Error Codes */
+#define RADIATOR_TEMPERATURE_GOOD 17
+#define RADIATOR_TEMPERATURE_HIGH 18
+#define RADIATOR_TEMPERATURE_LOW 19
 
 // Error Severity Levels
 #define INFO 1 // Everything is all gravy baby
@@ -53,15 +38,43 @@
 #define ERRORSTRING "##ERROR##"
 
 /**
+ * @function        throw_error_code
+ * @description     Generates an error message, and takes the necessary actions depending on the severity and error.
+ *                  Uses 0 as the part number
+ * @author          Shane Havers
+ * @date_created    24/06/2018
+ * @date_modified   N/A
+ * @param   severity	- Severity of the error (INFO, WARN, or ERROR)
+ * @param   error_code	- Error to report 
+ * @returns void  
+ * */
+void throw_error_code(uint16_t severity, uint16_t error_code);
+
+
+/**
+ * @function        throw_error_code_with_number
+ * @description     Generates an error message, and takes the necessary actions depending on the severity and error
+ * @author          Shane Havers
+ * @date_created    24/06/2018
+ * @date_modified   N/A
+ * @param   severity	- Severity of the error (INFO, WARN, or ERROR)
+ * @param   error_code	- Error to report
+ * @param   part_number - The number of the part generating the error (i.e. Gearbox 1, Radiator 2 etc.)
+ * @returns void  
+ * */
+void throw_error_code_with_number(uint16_t severity, uint16_t error_code, uint16_t part_number)
+
+/**
  * @function        send_error_message
  * @description     Formats a 50-character long log message, and sends it over UART
  * @author          Shane Havers
  * @date_created    23/06/2018
- * @date_modified   N/A
- * @param   char[]  start   - String representation of log level
- * @param   char[]  message - Message to send - should not exceed 40 characters
+ * @date_modified   24/06/2018
+ * @param   char[]      start       - String representation of log level
+ * @param   char[]      message     - Message to send - should not exceed 40 characters
+ * @param   uint16_t    part_number - The number of the part generating the error (i.e. Gearbox 1, Radiator 2 etc.)
  * @returns void
  * */
-void send_error_message(char start[], char message[]);
+void send_error_message(char start[], char message[], uint16_t part_number) 
 
 #endif // CHASSIS_ERROR_H
